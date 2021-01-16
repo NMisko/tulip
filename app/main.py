f3d68ae6af29a5119d455b695213519e264fd15a
Nicola Miskowiec <nicola.miskowiec@gmail.com>
2021-01-16 14:57:28 +0100

    feat: add tulip username validation endpoints

---
+++ app/main.py
@@ -0,0 +1,46 @@
+import random
+from datetime import datetime, timedelta
+from typing import Optional
+
+import names
+from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import time
+
+app = FastAPI()
+
+# Allow all origins
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def hello():
+    return "Hello 🌷"
+
+
+@app.get("/users/taken")
+def read_root(username):
+    if username == "tulip2014":
+        return {"taken": True}
+
+    if username.startswith("slow"):
+        time.sleep(4)
+
+    if username == "slow":
+        return {"taken": True}
+
+    if username == "error":
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    return {"taken": False}
